5816edfd639a61666003f1d206d7748c258378ac
johan577@mac.com <johan577@mac.com>
2026-07-02 11:16:26 +0200

    Suppress npm deprecation warnings in updVerlang.sh

--- updVerlang.sh
+++ updVerlang.sh
@@ -3,8 +3,8 @@ pushd /etc/nodejs/verlang3/
 git pull
 git reset --hard origin/main
 rm -rf node_modules
-npm ci
+npm ci --loglevel=error
 npm run build
-npm prune --omit=dev
+npm prune --omit=dev --loglevel=error
 pm2 restart verlang
 popd
